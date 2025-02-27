0cdbb46215e7e5cba96983c7fa8f4febb7502599
Jeree31 <jeremyallegratohartono_2327250004@mhs.mdp.ac.id>
2025-02-27 10:13:39 +0700

    27 feb 2023

--- pilem/lib/screens/home_screen.dart
+++ pilem/lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pilem/services/api_services.dart';
+import 'package:pilem/services/api_service.dart';
 import 'package:pilem/models/movie.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,14 +10,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final ApiServices _apiServices = ApiServices();
-  List<Movie> _allMovie = [];
+  final ApiService _apiServices = ApiService();
+  List<Movie> _allMovies = [];
+  List<Movie> _trendingMovies = [];
+  List<Movie> _popularMovies = [];
 
   Future<void> _loadMovies() async {
-    final List<Map<String, dynamic>> allMoviesData = await _apiServices.getAllMovies();
+    final List<Map<String, dynamic>> allMoviesData = 
+        await _apiServices.getAllMovies();
+        
+    final List<Map<String, dynamic>> trendingMovieData = 
+        await _apiServices.getTrendingMovies();
+
+    final List<Map<String, dynamic>> popularMovieData = 
+        await _apiServices.getPopularMovies();
 
     setState(() {
-      _allMovie = allMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _trendingMovies = trendingMovieData.map((e) => Movie.fromJson(e)).toList();
+      _popularMovies = popularMovieData.map((e) => Movie.fromJson(e)).toList();
     });
   }
 
@@ -32,37 +43,21 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Pilem",
-        ),
+        title: const Text("Pilem"),
       ),
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsets.all(8),
-              child: Text(
-                "All Movies",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 200,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: _allMovies.length,
-                itemBuilder:
-                itemBuilder: ,
-              )
-              
-            )
+            _buildMovieList("All Movies", _allMovies),
+            _buildMovieList("Trending Movies", _trendingMovies),
+            _buildMovieList("Popular Movies", _popularMovies),
           ],
+              )
         ),
-      ),
     );
   }
+
+@override
+widget _buildMovieList(String title, List<Movie>)
+
 }
